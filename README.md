65474d777c40103f1075b008ac13b7851a169248
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2023-09-17 00:28:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LegendOfWeapon
+ Weapon Fighting project
---
+++ Source/LegendOfWeapon/LegendOfWeapon.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LegendOfWeapon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LegendOfWeapon, "LegendOfWeapon" );
