a75137b7a80f8a13066e1ab2441620763655da55
Dahye Kim <57945707+ekgp3987@users.noreply.github.com>
2023-09-26 01:12:55 +0900

    feat : Create BPC, ABP
    1. Create Player Character Base, AnimInstance (Longsword, Spear)
    2. Create Player BPC, ABP

---
+++ Source/LegendOfWeapon/Character/AnimInst/AnimInstance_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AnimInstance_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEGENDOFWEAPON_API UAnimInstance_Base : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LegendOfWeapon/Character/AnimInst/Longsword_AnimInst_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimInstance_Base.h"
+#include "Longsword_AnimInst_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEGENDOFWEAPON_API ULongsword_AnimInst_Base : public UAnimInstance_Base
+{
+	GENERATED_BODY()
+	
+};
