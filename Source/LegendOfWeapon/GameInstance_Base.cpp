b7b3bb299289502aece047e1bc82a88d6de75ba4
Dahye Kim <57945707+ekgp3987@users.noreply.github.com>
2023-10-21 03:57:07 +0900

    feat : add GameInstance

---
+++ Source/LegendOfWeapon/GameInstance_Base.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameInstance_Base.h"
+
+
+UGameInstance_Base::UGameInstance_Base()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Hi Imgameinstance"));
+
+}
+
+UGameInstance_Base::~UGameInstance_Base()
+{
+
+}
