6563093d8a386dce82e3d01c70498336da8f7517
Dahye Kim <57945707+ekgp3987@users.noreply.github.com>
2023-11-03 00:36:59 +0900

    refactor : Light attack combo(BP->C++)

--- Source/LegendOfWeapon/Character/AnimInst/Spear_AnimInst_Base.h
+++ Source/LegendOfWeapon/Character/AnimInst/Spear_AnimInst_Base.h
@@ -13,5 +13,21 @@ UCLASS()
 class LEGENDOFWEAPON_API USpear_AnimInst_Base : public UAnimInstance_Base
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeBeginPlay() override;
+	virtual void NativeUpdateAnimation(float _fDeltaTime) override;
+
+
+public:
+	// AniNotify_ + 노티파이 이름 으로 함수명을 구성할 것, UFUNCTION() 매크로 반드시 붙일 것
+	UFUNCTION()
+	void AnimNotify_Attack1End();
+
+	UFUNCTION()
+	void AnimNotify_Attack2End();
+
+	UFUNCTION()
+	void AnimNotify_AttackEnd();
 };
--- Source/LegendOfWeapon/Character/LongswordCharacter_Base.cpp
+++ Source/LegendOfWeapon/Character/LongswordCharacter_Base.cpp
@@ -18,3 +18,17 @@ void ALongswordCharacter_Base::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
+
+void ALongswordCharacter_Base::LightAttack(const FInputActionInstance& _Instance)
+{	
+	// 해당 액션시 수행해야 할 코드 작성
+	UE_LOG(LogTemp, Warning, TEXT("Longsword Character LightAttack"));
+}
+
+void ALongswordCharacter_Base::MiddleAttack(const FInputActionInstance& _Instance)
+{
+}
+
+void ALongswordCharacter_Base::HeavyAttack(const FInputActionInstance& _Instance)
+{
+}
