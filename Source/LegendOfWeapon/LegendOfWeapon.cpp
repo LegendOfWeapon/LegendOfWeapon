2720ab5a7758d107c04b9d744921c2679da4e083
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2023-11-15 21:46:58 +0900

    Revert "Revert "Merge pull request #27 from LegendOfWeapon/DedicatedServer""
    This reverts commit ba56391ffddb905bc0195ce6bc12ec90733492ad.

--- Source/LegendOfWeapon/LegendOfWeapon.cpp
+++ Source/LegendOfWeapon/LegendOfWeapon.cpp
@@ -3,4 +3,6 @@
 #include "LegendOfWeapon.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(LogABNetwork);
+
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LegendOfWeapon, "LegendOfWeapon" );
